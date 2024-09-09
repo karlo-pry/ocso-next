683aa9d4bfd9b928277dd56dc58e94dce0f85c7d
karlo-pry <karlopazpory@gmail.com>
2024-09-08 18:06:51 -0600

    feat: Cookies in SSC

---
+++ app/dashboard/@count/error.tsx
@@ -0,0 +1,4 @@
+'use client'
+export default function ErrorCount() {
+  return 'Error al cargar tiendas'
+}
--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -1,7 +1,16 @@
+import { TOKEN_NAME } from "@/constants";
 import axios from "axios";
+import { cookies } from "next/headers";
 const CountPage = async () => {
-  const countLocations = await axios.get("http://127.0.0.1:4000/locations")
-  return "Hay tantas locations: " + countLocations?.data?.length;
+  const userCookies = cookies()
+  const token = userCookies.get(TOKEN_NAME)?.value
+  const countLocations = await axios.get("http://127.0.0.1:4000/locations", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  const cantidad = countLocations?.data?.length;
+  return `Hay: ${cantidad} tienda${cantidad > 1 ? "s" : ""}`
 }
 
 export default CountPage;
