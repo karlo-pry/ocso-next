c1d4ab376f1efdefc8c895d74250f4bc14f8a5f5
karlo-pry <karlopazpory@gmail.com>
2024-09-09 09:32:43 -0600

    feat: Layout /dashboard

--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -10,7 +10,7 @@ const CountPage = async () => {
     }
   })
   const cantidad = countLocations?.data?.length;
-  return `Hay: ${cantidad} tienda${cantidad > 1 ? "s" : ""}`
+  return <div className="w-2/12"> {`Hay: ${cantidad} tienda${cantidad > 1 ? "s" : ""}` } </div>
 }
 
 export default CountPage;
