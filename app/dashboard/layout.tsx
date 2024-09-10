740b7a097f763a1d98818101b661254c989e612c
karlo-pry <karlopazpory@gmail.com>
2024-09-10 10:32:39 -0600

    feat: Cards for employees location

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,10 +2,10 @@ import Header from "./_components/Header";
 import Sidebar from "./_components/_sidebar/Sidebar";
 export default function LayoutDashboard({
   children,
-  count,
+  locations,
 }: Readonly<{
   children: React.ReactNode;
-  count: React.ReactNode;
+  locations: React.ReactNode;
 }>) {
   return (
     <div className="bg-orange-50">
@@ -13,7 +13,7 @@ export default function LayoutDashboard({
       <div className="flex flex-row items-center">
         <Sidebar />
         {children}
-        {count}
+        {locations}
       </div>
     </div>
   );
