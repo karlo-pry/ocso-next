ebb9a2c71acea3448a6c436131e53d1cb2202cd5
karlo-pry <karlopazpory@gmail.com>
2024-09-14 13:07:51 -0600

    feat: Caching for delete and create

--- actions/locations/create.ts
+++ actions/locations/create.ts
@@ -1,7 +1,9 @@
 "use server";
 import { API_URL } from "@/constants";
+import { Location } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
 import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 export async function createLocation(formData: FormData) {
   let location: any = {};
   let locationLatLng = [0, 0];
@@ -22,8 +24,14 @@ export async function createLocation(formData: FormData) {
     method: "POST",
     body: JSON.stringify(location),
     headers: {
+      'content-type': 'application/json',
       ...authHeaders()
     }
   })
-  if (response.status === 201) revalidateTag("dashboard:locations");
+  const { locationId }: Location = await response.json()
+  if (response.status === 201) {
+    revalidateTag("dashboard:locations");
+    redirect(`/dashboard?store=${locationId}`)
+  }
+
 }
