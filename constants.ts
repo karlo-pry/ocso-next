319e7d8c2c1b45a59d445a6e2b6324107447e68e
karlo-pry <karlopazpory@gmail.com>
2024-09-27 19:41:25 -0600

    feat: PORT for API URL

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://ec2-54-163-182-22.compute-1.amazonaws.com/"
+export const API_URL = "https://ec2-54-163-182-22.compute-1.amazonaws.com:4000/"
 export const TOKEN_NAME = 'auth_for_ocso'
