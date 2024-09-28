33d3dc8a6202a111006ae683af5c178c6d2f7164
karlo-pry <karlopazpory@gmail.com>
2024-09-28 10:16:10 -0600

    https endpoint

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://ec2-54-163-182-22.compute-1.amazonaws.com:4000/"
+export const API_URL = "ec2-54-146-182-29.compute-1.amazonaws.com"
 export const TOKEN_NAME = 'auth_for_ocso'
